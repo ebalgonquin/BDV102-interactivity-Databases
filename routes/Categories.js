79eb7715c4f637673c4c8b53a92663c520e872d3
ebalgonquin <emilybethenriquez96@gmail.com>
2025-11-09 20:25:49 -0500

    updates

--- routes/Categories.js
+++ routes/Categories.js
@@ -1,27 +1,41 @@
-const {Category}=require('../models/Category');
-const express=require ('express');
-const router=express.Router;
+const { Category } = require('../models/Category');
+const express = require('express');
+const router = express.Router();
 
-router/get('/',async(req,res)=>{
-const CategoryList=await Category.find();
+//get all categories
+router.get('/', async (req, res) => {
+    const CategoryList = await Category.find();
 
-if (!CategoryList){
-    res.status(500).json({success:false})
-}
-res.send(CategoryList);
-})
+    if (!CategoryList) {
+        res.status(500).json({ success: false });
+    }
+    res.send(CategoryList);
+});
 
-router.post('/',async(req,res)=>{
-    //create new cateogry
-let Category=new Category ({
-    nane: req.body.name
-})
-Category= await Category.save();
-//if it works its saved if not then return failed
-if(!Category)
-    return res.status(404).send('category cannot be created')
+//create new cateogry
+router.post('/', async (req, res) => {
+    let Category = new Category({
+        name: req.body.name
+    });
 
-res.send(cateogry);
-})
+    Category = await Category.save();
+    //if it works its saved if not then return failed
+    if (!Category)
+        return res.status(404).send('category cannot be created');
 
-model.exports=router;
+    res.send(Category);
+});
+//deletes the category
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then(category => {
+        if (category) {
+            return res.status(200).json({ success: true, message: 'the cateogry is deleted' });
+        } else {
+            return res.status(404).json({ success: false, message: 'category note found' });
+        }
+    }).catch(err=>{
+        return res.status(400).json({success:false, error:err})
+    })
+});
+
+module.exports = router;
